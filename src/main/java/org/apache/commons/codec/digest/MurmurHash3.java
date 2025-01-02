70fed4c26cbf71839a09e3be3ec369cffd4efe35
Leonardo Falanga <info@leonardofalanga.it>
2025-01-02 16:27:53 +0100

    Solved second code smell

--- src/main/java/org/apache/commons/codec/digest/MurmurHash3.java
+++ src/main/java/org/apache/commons/codec/digest/MurmurHash3.java
@@ -937,6 +937,8 @@ public final class MurmurHash3 {
             k1 = Integer.rotateLeft(k1, R1_32);
             k1 *= C2_32;
             hash ^= k1;
+            break;
+        default: //nothing
         }
 
         hash ^= length;
